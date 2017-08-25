7bba61b680c1b55491f3e43adc58c56dc669ef76
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2017-08-25 18:24:05 +0900

    Relax the lifetime condition for

--- src/mutator.rs
+++ src/mutator.rs
@@ -254,7 +254,7 @@ impl Scope {
     }
 
     /// Deferred destruction and deallocation of heap-allocated object `ptr`.
-    pub unsafe fn defer_drop<T: Send + 'static>(&self, ptr: Ptr<T>) {
+    pub unsafe fn defer_drop<'s, T: Send + 's>(&'s self, ptr: Ptr<'s, T>) {
         self.defer_garbage(Garbage::new_drop(ptr.as_raw() as *mut T, 1))
     }
 
