4164e560536a5d8b8ad298ab72170125002168f4
Stjepan Glavina <stjepang@gmail.com>
2017-09-20 13:20:20 +0200

    Simplify and more tests

--- src/deferred.rs
+++ src/deferred.rs
@@ -1,58 +1,20 @@
 use std::mem;
 use std::ptr;
 
-/// Provides methods to dispatch a call to a `FnOnce()` from a trait object.
-pub trait Callback {
-    /// Calls the function from a trait object on the stack.
-    ///
-    /// This will copy `self`, call the function, and finally drop the copy.
-    /// This method may be called only once, and `self` must not be dropped after that (tip: pass
-    /// it to `std::mem::forget`).
-    unsafe fn copy_and_call(&self);
-
-    /// Calls the function from a trait object on the heap.
-    fn call_box(self: Box<Self>);
-}
-
-impl<F: FnOnce()> Callback for F {
-    #[inline]
-    unsafe fn copy_and_call(&self) {
-        let f: Self = ptr::read(self);
-        f();
-    }
-
-    #[inline]
-    fn call_box(self: Box<Self>) {
-        let f: Self = *self;
-        f();
-    }
-}
-
-/// The representation of a trait object like `&SomeTrait`.
+/// Number of words a piece of `Data` can hold.
 ///
-/// This struct has the same layout as types like `&SomeTrait` and `Box<AnotherTrait>`.
-///
-/// It is actually already provided as `std::raw::TraitObject` gated under the nightly `raw`
-/// feature. But we don't use nightly Rust, so the struct was simply copied over into Crossbeam.
-///
-/// If the layout of this struct changes in the future, Crossbeam will break, but that is a fairly
-/// unlikely scenario.
-// FIXME(stjepang): When feature `raw` gets stabilized, use `std::raw::TraitObject` instead.
-#[repr(C)]
-#[derive(Copy, Clone)]
-struct TraitObject {
-    data: *mut (),
-    vtable: *mut (),
-}
+/// Three words should be enough for the majority of cases. For example, you can fit inside it the
+/// function pointer together with a fat pointer representing an object that needs to be destroyed.
+const DATA_WORDS: usize = 3;
 
 /// Some space to keep a `FnOnce()` object on the stack.
-type Data = [usize; 3];
+type Data = [usize; DATA_WORDS];
 
 /// A `FnOnce()` that is stored inline if small, or otherwise boxed on the heap.
 ///
 /// This is a handy way of keeping an unsized `FnOnce()` within a sized structure.
 pub struct Deferred {
-    vtable: *mut (),
+    call: unsafe fn(*mut u8),
     data: Data,
 }
 
@@ -64,25 +26,26 @@ impl Deferred {
 
         unsafe {
             if size <= mem::size_of::<Data>() && align <= mem::align_of::<Data>() {
-                let vtable = {
-                    let callback: &Callback = &f;
-                    let obj: TraitObject = mem::transmute(callback);
-                    obj.vtable
-                };
-
-                let mut data = Data::default();
+                let mut data = mem::uninitialized();
                 ptr::write(&mut data as *mut Data as *mut F, f);
 
-                Deferred { vtable, data }
-            } else {
-                let mut data = Data::default();
-                let b: Box<Callback> = Box::new(f);
-                ptr::write(&mut data as *mut Data as *mut Box<Callback>, b);
-
-                Deferred {
-                    vtable: 1 as *mut (),
-                    data,
+                unsafe fn call<F: FnOnce()>(raw: *mut u8) {
+                    let f: F = ptr::read(raw as *mut F);
+                    f();
                 }
+
+                Deferred { call: call::<F>, data }
+            } else {
+                let b: Box<F> = Box::new(f);
+                let mut data = mem::uninitialized();
+                ptr::write(&mut data as *mut Data as *mut Box<F>, b);
+
+                unsafe fn call<F: FnOnce()>(raw: *mut u8) {
+                    let b: Box<F> = ptr::read(raw as *mut Box<F>);
+                    (*b)();
+                }
+
+                Deferred { call: call::<F>, data }
             }
         }
     }
@@ -90,39 +53,50 @@ impl Deferred {
     /// Calls the function or panics if it was already called.
     #[inline]
     pub fn call(&mut self) {
-        let vtable = mem::replace(&mut self.vtable, ptr::null_mut());
-        assert!(!vtable.is_null(), "cannot call `FnOnce` more than once");
+        unsafe fn fail(_: *mut u8) {
+            panic!("cannot call `FnOnce` more than once");
+        }
 
+        let call = mem::replace(&mut self.call, fail);
         unsafe {
-            if vtable as usize != 0x1 {
-                let data = &mut self.data as *mut _ as *mut ();
-                let obj = TraitObject { data, vtable };
-                let callback: &Callback = mem::transmute(obj);
-                callback.copy_and_call();
-            } else {
-                let b: Box<Callback> = ptr::read(&self.data as *const Data as *const Box<Callback>);
-                b.call_box();
-            }
+            call(&mut self.data as *mut Data as *mut u8);
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::cell::Cell;
     use super::Deferred;
 
     #[test]
-    fn smoke_on_stack() {
+    fn on_stack() {
+        let fired = &Cell::new(false);
         let a = [0usize; 1];
-        let mut d = Deferred::new(move || drop(a));
+
+        let mut d = Deferred::new(move || {
+            drop(a);
+            fired.set(true);
+        });
+
+        assert!(!fired.get());
         d.call();
+        assert!(fired.get());
     }
 
     #[test]
-    fn smoke_on_heap() {
+    fn on_heap() {
+        let fired = &Cell::new(false);
         let a = [0usize; 10];
-        let mut d = Deferred::new(move || drop(a));
+
+        let mut d = Deferred::new(move || {
+            drop(a);
+            fired.set(true);
+        });
+
+        assert!(!fired.get());
         d.call();
+        assert!(fired.get());
     }
 
     #[test]
