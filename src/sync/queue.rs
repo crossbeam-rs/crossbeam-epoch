f5403337e5a5e2485625828d5aa3b2dfce16aef5
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2017-11-24 21:24:48 +0900

    Rename into_ptr into into_shared

--- src/sync/queue.rs
+++ src/sync/queue.rs
@@ -55,7 +55,7 @@ impl<T> Queue<T> {
         });
         unsafe {
             let guard = &unprotected();
-            let sentinel = sentinel.into_ptr(guard);
+            let sentinel = sentinel.into_shared(guard);
             q.head.store(sentinel, Relaxed);
             q.tail.store(sentinel, Relaxed);
             q
@@ -92,7 +92,7 @@ impl<T> Queue<T> {
             data: ManuallyDrop::new(t),
             next: Atomic::null(),
         });
-        let new = Owned::into_ptr(new, guard);
+        let new = Owned::into_shared(new, guard);
 
         loop {
             // We push onto the tail, so we'll start optimistically by looking there first.
